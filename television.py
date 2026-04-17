2587a87a91c11f9e157c2ea3c41e480613deac22
DMilew <daniel.milewski@yahoo.com>
2026-04-16 21:35:17 -0500

    First file

---
+++ television.py
@@ -0,0 +1,35 @@
+
+
+
+class Television(self):
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+
+    def __init__(self):
+        __status = False
+        __muted = False
+        __volume = Television.MIN_VOLUME
+        __channel = Television.MIN_CHANNEL
+
+    def power(selfself):
+        pass
+
+    def mute(self):
+        pass
+
+    def channel_up(self):
+        pass
+
+    def channel_down(self):
+        pass
+
+    def volume_up(self):
+        pass
+
+    def volume_down(self):
+        pass
+
+    def __str__(self):
+        pass
